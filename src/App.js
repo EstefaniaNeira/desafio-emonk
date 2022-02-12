a2884b4372cbcbb62080e18c9823222ac679d5d8
Estefania Neira <pety7282@gmail.com>
2022-02-12 04:57:21 -0300

    Creacion de footer, limpie codigo y ajuste detalles de diseño

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import './index.css';
 function App() {
   return (
     <div className="container-fluid">
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar img-fluid mx-auto d-block m-1" href="App.js">
+      <nav className="navbar justify-content-center navbar-light bg-light">
+        <a className="navbar m-1" href="App.js">
           <img className="img-fluid i1" id="logo" src="https://res.cloudinary.com/esnese/image/upload/v1644525759/emonk_ixsvdk.png" alt="" />
         </a>
       </nav>
       <Producto />
+      <footer className="footer fixed-bottom mt-auto py-3 bg-light">
+        <p className="text-center text-muted">© 2022, Estefania Neira</p>
+      </footer>
     </div>
   );
 }
--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -16,38 +16,35 @@ const Producto = () => {
     return (
         <div className='container-fluid'>
             <div className="row">
-                <div className='col-md-6 mt-2'>
-                    <div className='mx-auto d-block m-5 border border-dark'>
-                        <img className='img-fluid' src="http://54.203.82.32:3000/api/productos/img/pd2-3-000002-001.jpg" alt="" />
-                    </div>
+                <div className='col-md-6'>
+                    <img className='img-fluid shadow-lg rounded-3 mx-auto d-block m-5 ' src="http://54.203.82.32:3000/api/productos/img/pd2-3-000002-001.jpg" alt="" />
                 </div>
-                <div className='col-md-6 mt-5'>
+                <div className='col-md-6 mt-3'>
                     <ul>
                         {!datos ? 'Cargando...' :
                             datos.producto.map((dato, index) => {
                                 return (
                                     <ul>
-                                        <h2>{dato.nombre}</h2>
-                                            <p className='text-muted font-weight-light'>Sku: {dato.sku}</p>
-                                            <p>{dato.descripcion}</p>
-                                        <h3 className='mt-2 mb-2'>Detalles del Producto</h3>
-                                            <li >Material: {dato.material}</li>
-                                            <li>Medidas: {dato.medidas}</li>
-                                            <li>Peso: {dato.peso_producto}</li>
-                                            <li>Color: {dato.color_diseno_panton}</li>
-                                        <h3 className='mt-2 mb-2'>Detalles del Packing</h3>
-                                            <li>Packing: {dato.packing_venta}</li>
-                                            <li>Medidas: {dato.medidas_ctn}</li>
-                                            <li>Peso: {dato.peso_ctn}</li>
+                                        <h2 className='border-bottom'>{dato.nombre}</h2>
+                                        <p className='text-muted font-weight-light'>Sku: {dato.sku}</p>
+                                        <p>{dato.descripcion}</p>
+                                        <h4 className='mt-2 mb-2'>Detalles del Producto</h4>
+                                        <li >Material: {dato.material}</li>
+                                        <li>Medidas: {dato.medidas}</li>
+                                        <li>Peso: {dato.peso_producto}</li>
+                                        <li>Color: {dato.color_diseno_panton}</li>
+                                        <h4 className='mt-2 mb-2'>Detalles del Packing</h4>
+                                        <li>Packing: {dato.packing_venta}</li>
+                                        <li>Medidas: {dato.medidas_ctn}</li>
+                                        <li>Peso: {dato.peso_ctn}</li>
+                                        <h4 className='mt-2 mb-2'>Formato</h4>
+                                        <li>{dato.formato}</li>
                                     </ul>
                                 )
                             })}
                     </ul>
                 </div>
             </div>
-
-
-
         </div>
     )
 }
